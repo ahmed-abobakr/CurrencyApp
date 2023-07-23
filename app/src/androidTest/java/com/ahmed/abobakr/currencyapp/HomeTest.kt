208b2d0899e54708695f9f22d88814661b909928
Ahmed.Abubakr <ah.abobakr88@gmail.com>
2023-07-23 17:19:14 +0300

    add failing UI converting currency Test

--- app/src/androidTest/java/com/ahmed/abobakr/currencyapp/HomeTest.kt
+++ app/src/androidTest/java/com/ahmed/abobakr/currencyapp/HomeTest.kt
@@ -2,12 +2,17 @@ package com.ahmed.abobakr.currencyapp
 
 import android.content.pm.ActivityInfo
 import android.view.inputmethod.EditorInfo
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -52,6 +57,19 @@ class HomeTest {
             .check(matches(isDisplayed()))
 
         assertDisplayed(R.id.btnDetails)
+    }
+
+    @Test
+    fun selectSpinnerAndChangeEditTextValueShouldDisplayCurrencyValue(){
+        onView(withId(R.id.spinnerFrom)).perform(click())
+        onData(allOf(instanceOf(String::class.java))).atPosition(1).perform(click())
+
+        onView(withId(R.id.spinnerTo)).perform(click())
+        onData(allOf(instanceOf(String::class.java))).atPosition(2).perform(click())
+
+        onView(withId(R.id.editFrom)).perform(typeText("100"))
+
+        onView(withId(R.id.editTo)).check(matches(withText("3.25")))
     }
 
 }
--- app/src/main/java/com/ahmed/abobakr/currencyapp/home/HomeFragment.kt
+++ app/src/main/java/com/ahmed/abobakr/currencyapp/home/HomeFragment.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.ahmed.abobakr.currencyapp.R
+import com.ahmed.abobakr.currencyapp.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.spinnerFrom.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line,
+                    resources.getStringArray(R.array.currencies_From))
+
+        binding.spinnerTo.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.currencies_To))
     }
 
 }
