c474692ae85d6f60229512aa6d103c44a0f4760b
Ahmed.Abubakr <ah.abobakr88@gmail.com>
2023-07-23 15:30:55 +0300

    add Spinner From

---
+++ app/src/androidTest/java/com/ahmed/abobakr/currencyapp/HomeTest.kt
@@ -0,0 +1,22 @@
+package com.ahmed.abobakr.currencyapp
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeTest {
+
+    val mActivityRule = ActivityScenarioRule(MainActivity::class.java)
+        @Rule get
+
+    @Test
+    fun displayAppDesignTest(){
+        assertDisplayed(R.id.spinnerFrom)
+        assertDisplayed(R.id.btnSwipe)
+    }
+
+}
---
+++ app/src/main/java/com/ahmed/abobakr/currencyapp/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.ahmed.abobakr.currencyapp.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ahmed.abobakr.currencyapp.R
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
